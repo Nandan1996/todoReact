b6f0504281f46233aee95ef0b41a9195f443f9e4
Nandan1996 <choudharynandan260@gmail.com>
2017-08-02 22:34:04 +0530

    Add support for persisting data in localstorage.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore,combineReducers } from 'redux';
-import {Provider, connect} from 'react-redux';
+import {Provider} from 'react-redux';
 
 import {default as todoApp} from './reducer/statereducers.js';
 import TodoApp from './component/app.component.js';
 
+import {loadState, saveState} from './service/localstorage.service.js';
+
+const persistedState = loadState();
+
+const store = createStore(todoApp,persistedState);
+store.subscribe(() =>{
+    saveState({
+        todos: store.getState().todos
+    });
+});
 ReactDOM.render(
-    <Provider store={createStore(todoApp)}><TodoApp/></Provider>,
+    <Provider store={store}><TodoApp/></Provider>,
     document.getElementById('root')
  );
