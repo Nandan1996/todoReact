064e6b1a6b6a08e0e33e08c18516948354415070
Nandan1996 <choudharynandan260@gmail.com>
2017-08-02 22:40:25 +0530

    Add unique todo id generator action creator.

--- src/action/action.js
+++ src/action/action.js
@@ -1,10 +1,10 @@
 import * as types from '../constant/actiontype.js';
-
+import {v4} from 'node-uuid';
 let nextTodoId = 0;
 export const addTodo = (value) => {
 	return {
         type: types.ADD_TODO,
-        id: nextTodoId++,
+        id: v4(),
         text: value
     };
 }
