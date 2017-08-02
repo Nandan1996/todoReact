1481a91aa36a6f7516d8d1f567db050c570cd64e
Nandan1996 <choudharynandan260@gmail.com>
2017-08-02 22:47:14 +0530

    dd throttle from lodash to prevent frequent calling of setState.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore,combineReducers } from 'redux';
 import {Provider} from 'react-redux';
+import throttle from 'lodash/throttle';
 
 import {default as todoApp} from './reducer/statereducers.js';
 import TodoApp from './component/app.component.js';
@@ -9,13 +10,14 @@ import TodoApp from './component/app.component.js';
 import {loadState, saveState} from './service/localstorage.service.js';
 
 const persistedState = loadState();
-
 const store = createStore(todoApp,persistedState);
-store.subscribe(() =>{
+
+store.subscribe(throttle(() =>{
     saveState({
         todos: store.getState().todos
     });
-});
+},1000));
+
 ReactDOM.render(
     <Provider store={store}><TodoApp/></Provider>,
     document.getElementById('root')
